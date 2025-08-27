227ba113f7ec6e17de021a3394d847266e12def6
Trapti Rathaur <84930051+traptirathaur@users.noreply.github.com>
2025-08-27 22:20:33 +0530

    Update Aggregate Functions.sql

--- SQL Advanced Topics/Window Functions Practices/Aggregate Functions.sql
+++ SQL Advanced Topics/Window Functions Practices/Aggregate Functions.sql
@@ -1,2 +1,22 @@
----Refer to main content
+---# refer the content from "Subqueries Practice" folder
 
+---Find total copies sold per book
+SELECT b.title,
+		SUM (s.copies_sold) OVER (PARTITION BY s.book_id) AS total_copies
+FROM sales s
+JOIN books b ON s.book_id = b.book_id;  
+
+/*---above query will return book_id 4 (Harry Potter and the Sorcerer's Stone) twice  because when we use 
+window fns like "SUM() Over(Partition BY column_name)", the rows are not groups. Hence, the aggregate value is repeated for every row in that partition. 
+So we will be using below query to get exact output
+*/
+SELECT DISTINCT t.title , t.total_copies
+FROM (
+	SELECT b.title,
+		SUM (s.copies_sold) OVER (PARTITION BY s.book_id) AS total_copies
+	FROM sales s
+	JOIN books b ON s.book_id = b.book_id
+) t;
+
+
+  
