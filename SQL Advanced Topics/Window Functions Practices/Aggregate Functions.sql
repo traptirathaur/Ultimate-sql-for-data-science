b02353807d4b0524459d7fb2cf24f249673ce45b
Trapti Rathaur <84930051+traptirathaur@users.noreply.github.com>
2025-08-28 22:29:51 +0530

    Update Aggregate Functions.sql

--- SQL Advanced Topics/Window Functions Practices/Aggregate Functions.sql
+++ SQL Advanced Topics/Window Functions Practices/Aggregate Functions.sql
@@ -1,6 +1,6 @@
 ---# refer the content from "Subqueries Practice" folder
 
----Find total copies sold per book
+---Query 1.1: Find total copies sold per book
 SELECT b.title,
 		SUM (s.copies_sold) OVER (PARTITION BY s.book_id) AS total_copies
 FROM sales s
@@ -17,6 +17,18 @@ FROM (
 	FROM sales s
 	JOIN books b ON s.book_id = b.book_id
 ) t;
+---Let's try a different approach
+---Query 1.2: Find total copies sold per book along with sales_date, copies_sold
+SELECT b.title, s.sales_date, s.copies_sold,
+	SUM(s.copies_sold) OVER(PARTITION BY s.book_id) AS total_copies_sold
+FROM sales s
+JOIN books b ON s.book_id = b.book_id;
+
+---Query 2: Find revenue for each sale and total revenue per book
+SELECT b.title, s.sales_date, (s.copies_sold*s.price_per_copy) AS revenue,
+	SUM(s.copies_sold*s.price_per_copy) OVER(PARTITION BY b.book_id) AS total_revenue
+FROM sales s
+JOIN books b ON s.book_id=b.book_id;
 
 
   
